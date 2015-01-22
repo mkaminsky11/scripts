8f191492bf4b48469c42a441a79ba90809872f0a
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-22 10:37:47 -0500

    Update users.js

--- nodejs/users.js
+++ nodejs/users.js
@@ -2,15 +2,29 @@ var run = require("./../general.js").run;
 var complex = require("./../general.js").complex;
 var fs = require("fs");
 
-/*
-CONFIGURING USERS
-========================
-1. GET A LIST OF ALL USERS ON COMPUTER
-2. READ auth.txt, IN WHICH A USER PASTED A LIST OF ALL THE NON-ADMIN USERS
-3. READ admin.txt, IN WHICH A USER PASTED A LIST OF ALL ADMIN USERS
-4. LIST ALL USERS NOT ON EITHER auth.txt OR ADMIN.txt
-    "you should remove: testuser"
-5. READ A LIST OF ALL THE ADMINS ON THE COMPUTER
-6. LIST ALL ADMINS NOT ON admin.txt
-    "testuser is not an admin"
-*/
+var users_on_comp = []; //all users on computer (excluding root)
+var all_auth = []; //all authorized users (admin + no)
+var admin_on_comp = []; //all admins on computer
+var admin_auth = []; //all authorized admins
+var not_admin_auth = []; //all authorized users not admins
+
+//FIRST, GET ALL USERS
+//==========================
+complex("cd /home; ls", function(out){
+    users_on_comp = out.split("\n");
+
+    //THEN, READ DATA
+    admin_auth = fs.readFileSync("../admin.txt",'utf8').trim().split("\n");
+    not_admin_auth = fs.readFileSync("../auth.txt",'uft8').trim().split("\n");
+    all_auth = admin_auth.concat(not_admin_auth);
+    
+    for(var i = 0; i < users_on_comp.length; i++){
+        var user = users_on_comp[i];
+        if(all_auth.indexOf(user) === -1){
+            console.log("YOU MAY NEED TO REMOVE: " + user);
+        }
+    }
+    
+    //for now, will have to handle admin stuff on your own
+    console.log("==============\nDON'T FORGET TO CHECK WHO IS AUTHORIZED TO BE AN ADMIN!");
+});
