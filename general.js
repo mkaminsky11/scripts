4af26ba4903924409feb5e077d9d72403b011cd5
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-22 07:49:42 -0500

    Update general.js

--- general.js
+++ general.js
@@ -1,3 +1,6 @@
+//THIS IS GENERAL STUFF
+//=========================
+
 var sys = require('sys')
 var exec = require('child_process').exec;
 
@@ -8,5 +11,15 @@ exports.run = function(command, callback){
 		}
 		sys.puts(stdout);
 		callback();
+	});
+};
+
+exports.complex = function(command, callback){
+	var child = exec(command, function(err, stdout, stderr){
+		if(err){
+			console.log("EXEC ERROR: " + err);
+		}
+		sys.puts(stdout);
+		callback(stdout);
 	});
 };
