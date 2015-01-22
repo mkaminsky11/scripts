15a01dd008e9e05d977f4c7b2be6a4fa0e9accc2
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-22 13:44:53 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -3,16 +3,22 @@ var run = require("./../general.js").run;
 //DOES BASIC UPGRADES
 //===================
 run("apt-get update -y", function(){
-	run("apt-get upgrade -y", function(){});
-	run("apt-get dist-update -y", function(){});
+	run("apt-get upgrade -y", function(){
+		console.log("basic updates done! updating dist!");
+		run("apt-get dist-upgrade -y", function(){
+			next();
+		});
+	});
 });
-
-console.log("basic updates done!");
 
 //NOW OTHER THINGS
 //==================
 //latest firfox, etc
-run("apt-get install libpam-cracklib nmap gufw rkhunter chkrootkit -y", function(){});
-run("apt-get remove firefox -y", function(){
-	run("apt-get install firefox -y", function(){});
-});
+function next(){
+	console.log("downloading tools!")
+	run("apt-get install libpam-cracklib nmap gufw rkhunter chkrootkit -y", function(){
+		run("apt-get remove firefox -y", function(){
+			run("apt-get install firefox -y", function(){});
+		});
+	});
+}
