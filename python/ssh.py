bc864a118f3ee05b9b2479dd8b3e0892e695189c
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-23 07:27:49 -0500

    Create ssh.py

---
+++ python/ssh.py
@@ -0,0 +1,21 @@
+'''
+TODO:
+=======
+/etc/ssh/sshd_config
+-----------------
+PermitEmptyPasswords no
+PermitRootLogin no
+UsePrivilegeSeparation yes
+SyslogFacility AUTH
+LogLevel INFO
+LoginGraceTime 120
+StrictModes yes
+ChallengeResponseAuthentication yes
+UsePAM yes
+Protocol 2
+DebianBanner no
+-----------------
+then,
+service sshd restart
+-----------------
+'''
