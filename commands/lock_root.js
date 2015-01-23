2f86ee2b7d70fcca2bb1d821ef5c1848652515eb
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-23 07:28:42 -0500

    remove javascript, all python

--- commands/lock_root.js
+++
@@ -1,7 +0,0 @@
-var run = require("./../general.js").run;
-var fs = require("fs");
-
-//IT'S OBVIOUS WHAT THIS DOES. IT'S SPLIT OFF FROM THE REST SO YOU DON'T RUN IT BY ACCIDENT
-//=======================
-//you're welcome
-run("passwd -dl root", function(){});
