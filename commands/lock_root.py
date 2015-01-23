4d74c76eb5f8642736380fac694ba4d093c304d8
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-23 12:24:24 -0500

    Update lock_root.py

--- commands/lock_root.py
+++ commands/lock_root.py
@@ -5,6 +5,6 @@ print "Cyberpatriot script starting..."
 #THIS LOCKS THE ROOT USER
 #===================
 #in a separate file to prevent accidents
-subprocess.call("passwd -dl root".split())
+subprocess.call("passwd -l root".split())
 
 print "root locked!"
