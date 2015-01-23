665dd957f8325e642f68d78304fa02aba58962f9
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-23 07:44:13 -0500

    Update ssh.py

--- commands/ssh.py
+++ commands/ssh.py
@@ -1,3 +1,48 @@
+import subprocess
+
+print "Cyberpatriot script starting..."
+
+#FIRST, MAKE A BACKUP
+#=======================
+subprocess.call("cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup".split())
+
+#THEN, READ IT
+#=====================
+file = open(" /etc/ssh/sshd_config","r+")
+text = file.read().strip("\n").split("\n")
+
+#REMOVE POTENTIALLY OFFENDING LINES
+#==================
+for i, line in enumerate(text):
+    if ("PermitEmptyPasswords" in line) == True:
+        text[i] = ""
+    elif ("PermitRootLogin" in line) == True:
+        text[i] = ""
+    elif ("UsePrivilegeSeparatio" in line) == True:
+        text[i] = ""
+    elif ("SyslogFacility" in line) == True:
+        text[i] = ""
+    elif ("LogLevel" in line) == True:
+        text[i] = ""
+    elif ("LoginGraceTime" in line) == True:
+        text[i] = ""
+    elif ("StrictModes" in line) == True:
+        text[i] = ""
+    elif ("ChallengeResponseAuthentication" in line) == True:
+        text[i] = ""
+    elif ("UsePAM" in line) == True:
+        text[i] = ""
+    elif ("Protocol" in line) == True:
+        text[i] = ""
+    elif ("DebianBanner" in line) == True:
+        text[i] = ""
+
+#ADD CORRECT LINES
+#===================
+text.append("")
+text.append("")
+text.append("")
+
 '''
 TODO:
 =======
