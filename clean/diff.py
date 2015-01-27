b5aefb434d27c1db506736b3240bd75930382854
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-26 21:13:01 -0500

    edit list of default packages

--- clean/diff.py
+++ clean/diff.py
@@ -4,9 +4,7 @@ print "script starting..."
 
 #LISTS OF STUFF ON CLEAN INSTALL
 #====================
-clean1204 = """
-            
-            """
+
 
 #FIRST, GET ALL THINGS INSTALLED ON THIS COMPUTER
 #===================
