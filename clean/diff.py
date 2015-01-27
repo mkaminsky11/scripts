d3119a6c05e625f264810229b6efae454dae08fc
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-27 12:22:35 -0500

    finish diff

--- clean/diff.py
+++ clean/diff.py
@@ -4,7 +4,16 @@ print "script starting..."
 
 #LISTS OF STUFF ON CLEAN INSTALL
 #====================
-
+f = open(filename, 'r+')
+clean = f.read().split("\n")
 
 #FIRST, GET ALL THINGS INSTALLED ON THIS COMPUTER
 #===================
+p = subprocess.Popen("dpkg --get-selections | grep -v deinstall", stdout=subprocess.PIPE, shell=True)
+out,erro = p.communicate()
+
+installed = out.split("\n")
+for line in installed:
+    new_line = line[::-1].replace('llatsni', '', 1)[::-1].strip()
+    if (new_line in clean) == False:
+        print "You may want to remove: " + new_line
