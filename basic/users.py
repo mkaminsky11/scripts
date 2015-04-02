63251d2cdd20c4cb5d2933caaccede345041c40c
Michael Kaminsky <mkaminsky11@gmail.com>
2015-04-02 11:19:40 -0400

    Update users.py

--- basic/users.py
+++ basic/users.py
@@ -5,30 +5,43 @@ readme = open("../readme.txt","r+")
 text = readme.read()
 readme.close()
 
-authorize_users_text = text.split("Authorized Administrators:\n")[1].split("Authorized Users:")[0].strip().split("\n")
+#get all authorized admins
+authorize_admins_text = text.split("Authorized Administrators:\n")[1].split("Authorized Users:")[0].strip().split("\n")
+actual_authorized_admins = []
+for line in authorized_admins_text:
+    if ("password" in line) == False:
+        actual_authorized_admins.append(line.replace(" (you)","").strip())
+#get all authorized users
+authorize_users_text = text.split("Authorized Users:\n")[1].split("Do not remove any authorized users or their home")[0].strip().split("\n")
 actual_authorized_users = []
 for line in authorized_users_text:
-    if ("password" in line) == False:
-        actual_authorized_users.append(line.replace(" (you)",""))
-        #TODO: test this + compare with list of users on computer
-
-#REMOVING THIS BECAUSE IT NEEDS TO BE REDONE
+    if line.strip() !== "":
+        actual_authorized_users.append(line.strip())
 
 #FIRST GET ALL USERS
 #=====================
 # this gets all human users
-# before, just got all in "/home"
 p = subprocess.Popen("cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1".split(), stdout=subprocess.PIPE)
 output, err = p.communicate()
 users_on_computer = output.split("\n")
 
+#THEN, GET ALL ADMINS
+#====================
+admins_on_computer = []
 sudo_groups = ["sudo", "su", "sudoers", "wheel", "staff"]
 for group in sudo_groups:
-    get_users_in_group(group)
+    group_array = get_users_in_group(group)
+    for array_item in group_array:
+        if (array_item in admins_on_computer) == False:
+            admins_on_computer.append(array_item)
+
+#TODO: compare on computer vs. authorized
 
 def get_users_in_group(groups):
-    #TODO
-    #command: getent group groupname | awk -F: '{print $4}'
+    #TODO: test this
+    p = subprocess.Popen("getent group groupname | awk -F: '{print $4}'".split(), stdout=subprocess.PIPE)
+    output, err = p.communicate()
+    return output.split("\n")
 
 #THEN LOCK ROOT
 #=============
