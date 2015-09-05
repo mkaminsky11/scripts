13dfe13c7cea3a3d20e486a4949b49d54440ba0f
Michael Kaminsky <mkaminsky11@gmail.com>
2015-09-05 19:35:02 -0400

    Update bash.py

--- config/bash.py
+++ config/bash.py
@@ -1,5 +1,38 @@
+import subprocess
+import os.path
+
 #CONFIGURES BASH STUFF
+
+#TODO: maybe to all users?
 
 #PREVENT TAMPERING WITH THESE FILES
 append_only = [".bash_history",".bash_profile",".bash_login",".profile",".bash_logout",".bashrc"]
+for appends in append_only:
+    subprocess.call(("chattr +a ~/" + appends).split()) #set to append only
+    
+if os.path.exists("~/.bashrc") == False:
+    #want to create it
+    subprocesses.call("touch ~/.bashrc".split())
+    
+file = open("~/.bashrc","r+")
+text = file.read().strip("\n").split("\n")
 
+text.append("shopt -s histappend")
+text.append("readonly PROMPT_COMMAND="history -a" ")
+text.append("readonly HISTFILE")
+text.append("readonly HISTFILESIZE")
+text.append("readonly HISTSIZE")
+text.append("readonly HISTCMD")
+text.append("readonly HISTCONTROL")
+text.append("readonly HISTIGNORE")
+
+text = '\n'.join([str(x) for x in text])
+file.seek(0)
+file.write(text)
+file.truncate()
+file.close()
+
+#DISABLE OTHER SHELLS
+subprocesses.call("chmod 750 csh".spit())
+subprocesses.call("chmod 750 tcsh ".spit())
+subprocesses.call("chmod 750 ksh".spit())
